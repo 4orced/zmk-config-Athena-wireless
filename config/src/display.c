3540ddd3017fb2e461f4141b90330232061a71a0
Logan D. <reload@live.com>
2025-07-12 08:27:41 -0700

    Add custom status screen and logo rendering setup

--- config/src/display.c
+++ config/src/display.c
@@ -1,26 +1,21 @@
-```c
 #include <zmk/display/status.h>
-#include <zmk/log.h>
 #include <lvgl.h>
 #include "logo.h"
 
 LV_IMG_DECLARE(my_logo_img);
 
 static void render_right(lv_obj_t *parent) {
-    LOG_DBG("Rendering RIGHT display...");
     lv_obj_t *img = lv_img_create(parent);
     lv_img_set_src(img, &my_logo_img);
     lv_obj_center(img);
 }
 
 static void render_left(lv_obj_t *parent) {
-    LOG_DBG("Rendering LEFT display...");
     zmk_display_status_init();
     zmk_display_status_render(parent);
 }
 
 int zmk_display_render_status() {
-    LOG_DBG("zmk_display_render_status() called");
 #if IS_ENABLED(CONFIG_ZMK_DISPLAY_RIGHT)
     render_right(lv_scr_act());
 #elif IS_ENABLED(CONFIG_ZMK_DISPLAY_LEFT)
--- config/src/logo.h
+++ config/src/logo.h
@@ -1,5 +1,3 @@
-```c
 #pragma once
 #include <lvgl.h>
 extern const lv_img_dsc_t my_logo_img;
-```
