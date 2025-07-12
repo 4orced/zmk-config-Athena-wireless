5ca73023bbb466382fdb3cfb57040992ddcc5e7a
Logan D. <reload@live.com>
2025-07-11 21:43:24 -0700

    Clean rebuild of custom display rendering for Nice!View

--- config/src/display.c
+++ config/src/display.c
@@ -1,11 +1,11 @@
+```c
 #include <zmk/display/status.h>
-#include <zmk/log.h> // <-- Add this line
+#include <zmk/log.h>
 #include <lvgl.h>
 #include "logo.h"
 
-LV_IMG_DECLARE(my_logo_img); // Defined in logo.c
+LV_IMG_DECLARE(my_logo_img);
 
-// Right-side: show logo
 static void render_right(lv_obj_t *parent) {
     LOG_DBG("Rendering RIGHT display...");
     lv_obj_t *img = lv_img_create(parent);
@@ -13,24 +13,18 @@ static void render_right(lv_obj_t *parent) {
     lv_obj_center(img);
 }
 
-// Left-side: show battery + WPM
 static void render_left(lv_obj_t *parent) {
     LOG_DBG("Rendering LEFT display...");
     zmk_display_status_init();
     zmk_display_status_render(parent);
 }
 
-// ZMK render hook
 int zmk_display_render_status() {
     LOG_DBG("zmk_display_render_status() called");
 #if IS_ENABLED(CONFIG_ZMK_DISPLAY_RIGHT)
-    LOG_DBG("CONFIG_ZMK_DISPLAY_RIGHT is enabled");
     render_right(lv_scr_act());
 #elif IS_ENABLED(CONFIG_ZMK_DISPLAY_LEFT)
-    LOG_DBG("CONFIG_ZMK_DISPLAY_LEFT is enabled");
     render_left(lv_scr_act());
-#else
-    LOG_WRN("No display side is enabled!");
 #endif
     return 0;
 }
--- config/src/logo.h
+++ config/src/logo.h
@@ -1,3 +1,5 @@
+```c
 #pragma once
 #include <lvgl.h>
 extern const lv_img_dsc_t my_logo_img;
+```
